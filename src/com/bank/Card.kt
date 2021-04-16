4cd7b0071f29ea31a60dbcc220663a6e149f0368
Sruthi1498 <55286580+Sruthi1498@users.noreply.github.com>
2021-04-16 12:44:07 +0530

    Add files via upload

---
+++ src/com/bank/Card.kt
@@ -0,0 +1,3 @@
+package com.bank
+
+data class Card(val cardId: Int, val UserId: Int, val number:String,val pin:Int)
---
+++ src/com/bank/User.kt
@@ -0,0 +1,3 @@
+package com.bank
+
+data class User (val UserId:Int,val name: String)
